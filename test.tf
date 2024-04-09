4aa1d044f5ac59dbe91aa2f9d88896d4af10ac6a
Matthias Kay <matthias.kay@hlag.com>
2024-04-09 12:07:25 +0200

    Update test.tf

--- test.tf
+++ test.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "example" {
 }
 
 data "aws_lambda_layer_version" "datadog_extension" {
-  layer_name = "arn:aws:lambda:us-east-1:464622532012:layer:Datadog-Extension"
+  layer_name = "arn:aws:lambda:us-east-1:464622532012:layer:Datadog-Extension1"
   # renovate: datasource=aws-lambda-layer filter={"name": "my-layer", "architecture": "x86_64", "runtime": "python3.7"}
   version = 34
 }
